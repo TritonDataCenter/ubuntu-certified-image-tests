eae3d111983ef30a8d406a5a1721cf1d546b1502
Christopher Horrell <christopher@joyent.com>
2014-02-19 00:49:28 +0000

    Renamed product_file_spec.rb to product_and_branding_files_spec.rb

--- spec/ubuntu/product_and_branding_files_spec.rb
+++ spec/ubuntu/product_and_branding_files_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
-# Files that should be installed
-
+# Product file that lists image and version info
 describe file('/etc/product') do
 	it { should be_file }
 	it { should contain "Name: Joyent Instance" }
@@ -9,3 +8,8 @@ describe file('/etc/product') do
 	it { should contain "Documentation: #{attr[:doc_url]}" }
 end
 
+# Version info used for MOTD
+#describe file('/etc/cloud/build.info') do
+#  it { should be_file }
+#	it { should contain "serial: #{attr[:version]}" }
+#end
