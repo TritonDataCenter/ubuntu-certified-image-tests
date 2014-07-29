f00fa29bf20006ca30e122482b1b40049066b7c1
Christopher Horrell <christopher@joyent.com>
2014-07-29 13:51:21 +0000

    Test for 5 second grub timout. Check proper grub file

--- spec/ubuntu/grub.rb
+++ spec/ubuntu/grub.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
-describe file('/etc/default/grub.d/50-cloudimg-settings.cfg') do
+describe file('/etc/default/grub') do
 	it { should be_file }
 	it { should contain "GRUB_CMDLINE_LINUX_DEFAULT="tsc=reliable earlyprintk"" }
+  it { should contain "GRUB_TIMEOUT=5" }
 end
 
 
