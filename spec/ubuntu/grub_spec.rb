66c6a4ec42afb096e0ed39fa494358788e264942
Christopher Horrell <christopher@joyent.com>
2014-08-08 14:48:58 +0000

    test for grub settings in 50-cloudimg-settings.cfg

--- spec/ubuntu/grub_spec.rb
+++ spec/ubuntu/grub_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe file('/etc/default/grub') do
+describe file('/etc/default/grub.d/50-cloudimg-settings.cfg') do
 	it { should be_file }
 	it { should contain "GRUB_CMDLINE_LINUX_DEFAULT=\"tsc=reliable earlyprintk\"" }
   it { should contain "GRUB_TIMEOUT=5" }
