bde5d5669b0ccf679f17354fb8635adc9fa63768
Christopher Horrell <christopher@horrell.ca>
2016-02-10 10:55:55 -0500

    Add release property. Test for specific grub config according to release

--- spec/ubuntu/grub_spec.rb
+++ spec/ubuntu/grub_spec.rb
@@ -2,8 +2,19 @@ require 'spec_helper'
 
 describe file('/etc/default/grub.d/50-cloudimg-settings.cfg') do
 	it { should be_file }
-	it { should contain "GRUB_CMDLINE_LINUX_DEFAULT=\"tsc=reliable earlyprintk" }
   it { should contain "GRUB_TIMEOUT=5" }
 end
 
+
+# net.ifnames=0 was added to 16.04 to ensure the image uses eth(0-9) interface 
+# naming
+if property[:release] >= 16.04
+  describe file('/etc/default/grub.d/50-cloudimg-settings.cfg') do
+    it { should contain "GRUB_CMDLINE_LINUX_DEFAULT=\"tsc=reliable earlyprintk net.ifnames=0\"" }
+  end
+else
+  describe file('/etc/default/grub.d/50-cloudimg-settings.cfg') do
+    it { should contain "GRUB_CMDLINE_LINUX_DEFAULT=\"tsc=reliable earlyprintk\"" }
+  end
+end
 
