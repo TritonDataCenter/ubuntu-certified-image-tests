2a54091ed988adbc41868ee14209ed036a19144b
Christopher Horrell <christopher@joyent.com>
2016-09-12 17:16:38 -0400

    Fix tests for new cloud-init networking

--- spec/ubuntu/grub_spec.rb
+++ spec/ubuntu/grub_spec.rb
@@ -5,17 +5,6 @@ describe file('/etc/default/grub.d/50-cloudimg-settings.cfg') do
   it { should contain "GRUB_TIMEOUT=5" }
 end
 
-
-# This will change for the final 16.04 image and the testing logic should be modified.
-# net.ifnames=0 was added to 16.04 to ensure the image uses eth(0-9) interface 
-# naming
-if property[:release] >= 16.04
-  describe file('/etc/default/grub.d/50-cloudimg-settings.cfg') do
-    it { should contain "GRUB_CMDLINE_LINUX_DEFAULT=\"tsc=reliable earlyprintk net.ifnames=0\"" }
-  end
-else
-  describe file('/etc/default/grub.d/50-cloudimg-settings.cfg') do
-    it { should contain "GRUB_CMDLINE_LINUX_DEFAULT=\"tsc=reliable earlyprintk\"" }
-  end
+describe file('/etc/default/grub.d/50-cloudimg-settings.cfg') do
+  it { should contain "GRUB_CMDLINE_LINUX_DEFAULT=\"tsc=reliable earlyprintk\"" }
 end
-
--- spec/ubuntu/interfaces_spec.rb
+++ spec/ubuntu/interfaces_spec.rb
@@ -3,22 +3,44 @@ require 'spec_helper'
 # This test requires a VM to be provision with two IPs, preferably one public
 # and one private.
 
-# Test to ensure the VM has two interfaces, eth0 and eth1
+# Test to ensure the VM has two interfaces
+# Older images use eth0 and eth1, 16.04 and up use net0 and net1
 
-# eth0
-describe interface('eth0') do
-  it { should exist }
-end
 
-describe interface('eth0') do
-  it { should be_up }
-end
+if property[:release] >= 16.04
+    # net0
+    describe interface('net0') do
+      it { should exist }
+    end
 
-# eth1
-describe interface('eth1') do
-  it { should exist }
-end
+    describe interface('net0') do
+      it { should be_up }
+    end
 
-describe interface('eth1') do
-  it { should be_up }
+    # net1
+    describe interface('net1') do
+      it { should exist }
+    end
+
+    describe interface('net1') do
+      it { should be_up }
+    end
+else
+    # eth0
+    describe interface('eth0') do
+      it { should exist }
+    end
+
+    describe interface('eth0') do
+      it { should be_up }
+    end
+
+    # eth1
+    describe interface('eth1') do
+      it { should exist }
+    end
+
+    describe interface('eth1') do
+      it { should be_up }
+    end
 end
